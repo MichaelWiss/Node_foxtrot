70e68be79e2b20c9083354ad034ff5ea2fca38c8
Michael Wiss <michael.wiss@gmail.com>
2015-10-07 09:04:51 -0500

    required video.js module

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var videos = require('./routes/videos');
 
 var app = express();
 
